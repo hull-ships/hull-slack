55e464a0e45f29890eea09149008c960da4e4793
Romain Dardour <romain@hull.io>
2016-02-04 09:18:16 +0100

    remove comments

--- server/update-segment.js
+++ server/update-segment.js
@@ -1,10 +1,4 @@
 // import _ from 'lodash';
 
 export default function(notification={}, context={}){
-  // try {
-  //  var analytics = new Analytics(ship.settings.write_key);
-  //  analytics.track({ event: 'Update Segments', properties });
-  //   return true
-  // } catch(err) {
-  // }
 }
