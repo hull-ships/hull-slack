bd8c411c2c3f7641cc91f36ac45c66bc86b9bac1
Romain Dardour <romain@hull.io>
2016-07-21 11:32:59 +0200

    cleanup

--- server/index.js
+++ server/index.js
@@ -1,18 +1,9 @@
 import Hull from "hull";
-import winstonLogzio from "winston-logzio";
-// import winstonSlacker from "winston-slacker";
 import Server from "./server";
-
-require("dotenv").config();
-
 
 if (process.env.NODE_ENV === "development") {
   Hull.logger.transports.console.level = "debug";
 }
-
-// Post to Slack Channel directly.
-// Hull.logger.add(winstonSlacker,  { webhook, channel, username, iconUrl, iconImoji, customFormatter });
-// 
 
 Server({
   Hull,
