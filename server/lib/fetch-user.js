faf576c08abf838408d8809a3beb35762351c5c6
Romain Dardour <romain@hull.io>
2016-07-22 20:09:51 +0200

    cleanup

--- server/lib/fetch-user.js
+++ server/lib/fetch-user.js
@@ -2,7 +2,7 @@ import _ from "lodash";
 import queries from "./queries";
 
 module.exports = function fetchUser({ hull, search }) {
-  const { groups, email, name } = search;
+  const { email, name } = search;
   const params = (email) ? queries.email(email) : queries.name(name);
   return hull.post("search/user_reports", params)
   .then(({ pagination = {}, data = [] }) => {
@@ -12,7 +12,7 @@ module.exports = function fetchUser({ hull, search }) {
       .then((segments) => { return { segments, pagination, user }; });
   })
   .then(({ segments, pagination, user }) => {
-    let groupedUser = hull.utils.groupTraits(_.omitBy(user, v => (v === null || v === "" || v === undefined)));
+    const groupedUser = hull.utils.groupTraits(_.omitBy(user, v => (v === null || v === "" || v === undefined)));
     return { hull, user: groupedUser, segments, pagination };
   });
 };
