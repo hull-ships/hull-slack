0b78d07c876d85bb56a4ad108d1f6292b123290b
Romain Dardour <romain@hull.io>
2016-09-14 17:51:22 +0200

    reactivate log levels

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
 import Hull from "hull";
 import Server from "./server";
 
-// if (process.env.LOG_LEVEL) {
-//   Hull.logger.transports.console.level = process.env.LOG_LEVEL;
-// }
+if (process.env.LOG_LEVEL) {
+  Hull.logger.transports.console.level = process.env.LOG_LEVEL;
+}
 
 Server({
   Hull,
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
   hostSecret: process.env.SECRET || "1234",
-  // devMode: process.env.NODE_ENV === "development",
+  devMode: process.env.NODE_ENV === "development",
   port: process.env.PORT || 8082
 });
